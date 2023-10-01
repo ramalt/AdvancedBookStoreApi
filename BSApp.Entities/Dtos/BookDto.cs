ae30c17373b792850398d1716678233c30bda46b
ramalt <ramazanalltuntepe@gmail.com>
2023-10-01 17:10:39 +0300

    refactor

--- BSApp.Entities/Dtos/BookDto.cs
+++ BSApp.Entities/Dtos/BookDto.cs
@@ -1,6 +1,6 @@
 namespace BSApp.Entities.Dtos;
 public record BookDto
 {
-    public string Title { get; init; }
+    public string Title { get; init; } = null!;
     public decimal Price { get; init; }
 }
