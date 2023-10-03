660f58c93d09b25a912d92cf27e3a92a06297e60
ramalt <ramazanalltuntepe@gmail.com>
2023-10-03 18:29:44 +0300

    refactor

--- BSApp.Entities/RequestFeatures/BookParameters.cs
+++ BSApp.Entities/RequestFeatures/BookParameters.cs
@@ -3,7 +3,7 @@ namespace BSApp.Entities.RequestFeatures;
 public class BookParameters : RequestParameterBase
 {
     public uint MinPrice { get; set; }
-    public uint MaxPrice { get; set; }
+    public uint MaxPrice { get; set; } = 1000;
     public string? SearchTerm { get; set; }
     public bool ValidPriceRange => MaxPrice > MinPrice;
 
