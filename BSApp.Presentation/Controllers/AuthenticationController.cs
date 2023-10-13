4b439330044a4dd0fa33425bd62b4f91d9c3f846
ramalt <ramazanalltuntepe@gmail.com>
2023-10-14 00:05:56 +0300

    feature: Auth controller created.

---
+++ BSApp.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+using BSApp.Entities.Dtos.User;
+using BSApp.Presentation.ActionFilters;
+using BSApp.Service.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSApp.Presentation.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public AuthenticationController(IServiceManager service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> RegisterUser([FromBody] RegisterUserDto registerUserDto){
+        var result = await _service.AuthenticationService.RegisterUser(registerUserDto);
+
+        if (!result.Succeeded)
+        {
+            foreach (var err in result.Errors)
+            {
+                ModelState.TryAddModelError(err.Code, err.Description);    
+            }
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
+
+    }
+}
